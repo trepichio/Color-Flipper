82bbb6f579a9dae2cddf4793aed68824f03aab15
trepichio <trepichio@gmail.com>
2021-04-09 19:23:43 -0300

    move handlers code to named functions

--- src/main.js
+++ src/main.js
@@ -9,33 +9,35 @@ function hexHandler() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16)
 }
 
-const modes = {
-  'simple': simpleHandler,
-  'hex': hexHandler,
-}
-
-
-const links = document.querySelector('.nav-links')
-
-links.addEventListener('click', (e) => {
+function toggleLinksHandler(e) {
   if (e.target === links) return
 
   Array.from(links.children).forEach(link => {
     link.classList.remove('link-active')
     if (link === e.target) e.target.classList.add('link-active');
   })
-})
+
+}
+
+function changeBackgroundColorHandler(e) {
+    const root = document.documentElement
+    const colorTag = document.querySelector('#color-tag')
+
+    const chosenMode = document.querySelector('.nav-links .link.link-active').textContent.toLowerCase()
+
+    const randomColor = modes[chosenMode]()
+
+    colorTag.textContent = randomColor
+    root.style.setProperty('--background-chosen-color', randomColor)
+}
+
+const modes = {
+  'simple': simpleHandler,
+  'hex': hexHandler,
+}
+
+const links = document.querySelector('.nav-links')
+links.addEventListener('click', toggleLinksHandler)
 
 const btnClickMe = document.querySelector('#click-me')
-
-btnClickMe.addEventListener('click', (e) => {
-  const root = document.documentElement
-  const colorTag = document.querySelector('#color-tag')
-
-  const chosenMode = document.querySelector('.nav-links .link.link-active').textContent.toLowerCase()
-
-  const randomColor = modes[chosenMode]()
-
-  colorTag.textContent = randomColor
-  root.style.setProperty('--background-chosen-color', randomColor)
-})
+btnClickMe.addEventListener('click', changeBackgroundColorHandler)
