07e28b14b22e64d42ddfb976c32d79724c1e5651
trepichio <trepichio@gmail.com>
2021-04-09 15:54:09 -0300

    add random color click event (only hex)

--- src/main.js
+++ src/main.js
@@ -8,3 +8,18 @@ links.addEventListener('click', (e) => {
     if (link === e.target) e.target.classList.add('link-active');
   })
 })
+
+const getRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+const btnClickMe = document.querySelector('#click-me')
+
+btnClickMe.addEventListener('click', (e) => {
+  const root = document.documentElement
+  const colorTag = document.querySelector('#color-tag')
+
+  const randomColor = getRandomColor()
+  colorTag.textContent = randomColor
+  root.style.setProperty('--background-chosen-color', randomColor)
+})
