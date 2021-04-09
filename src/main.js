fb4d447cb6be9b358379c5bc3012cf506937e173
trepichio <trepichio@gmail.com>
2021-04-09 18:33:29 -0300

    add hex and simple mode handlers

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+function simpleHandler() {
+  const colors = ['green', 'red', 'rgba(125, 243,155)', '#f1523ffe']
+
+  const randomNumber = Math.floor(Math.random() * colors.length)
+  return colors[randomNumber]
+}
+
+function hexHandler() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+const modes = {
+  'simple': simpleHandler,
+  'hex': hexHandler,
+}
+
+
 const links = document.querySelector('.nav-links')
 
 links.addEventListener('click', (e) => {
@@ -9,17 +26,16 @@ links.addEventListener('click', (e) => {
   })
 })
 
-const getRandomColor = () => {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16)
-}
-
 const btnClickMe = document.querySelector('#click-me')
 
 btnClickMe.addEventListener('click', (e) => {
   const root = document.documentElement
   const colorTag = document.querySelector('#color-tag')
 
-  const randomColor = getRandomColor()
+  const chosenMode = document.querySelector('.nav-links .link.link-active').textContent.toLowerCase()
+
+  const randomColor = modes[chosenMode]()
+
   colorTag.textContent = randomColor
   root.style.setProperty('--background-chosen-color', randomColor)
 })
